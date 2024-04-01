ffaf1329abc595edcc91fd12dbc21aab5e569679
m-o-r-v-o <8584184+m-o-r-v-o@users.noreply.github.com>
2024-04-01 17:18:57 -0500

    working on styling

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function checkPasswords() {
   if (document.getElementById('password').value == document.getElementById('confirmPassword').value) {
     document.getElementById('passwordMessage').style.color = 'green';
-    document.getElementById('passwordMessage').innerHTML = 'Passwords Match';
+    document.getElementById('password').style.borderColor = 'green';
+    document.getElementById('confirmPassword').style.borderColor = 'green';
+    document.getElementById('passwordMessage').innerHTML = '';
   }
   else {
     document.getElementById('passwordMessage').style.color = 'red';
+    document.getElementById('password').style.borderColor = 'red';
+    document.getElementById('confirmPassword').style.borderColor = 'red';
     document.getElementById('passwordMessage').innerHTML = 'Passwords Do Not Match';
   }
 }
